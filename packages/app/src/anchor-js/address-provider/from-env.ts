511401471ced709cd4d9486cec147450b5b9a8d5
jess <jesse@soob.co>
2020-11-26 18:39:32 +0900

    fix(anchor-js/address-provider): env

--- packages/app/src/anchor-js/address-provider/from-env.ts
+++ packages/app/src/anchor-js/address-provider/from-env.ts
@@ -1,39 +1,39 @@
+import { reactifyEnv } from "../../libs/react-app-prefix"
+
 export class AddressProviderFromEnvVar implements AddressProvider.Provider {
   bAssetReward(denom: string): string {
-    return "terra1"
-    throw new Error('Method not implemented.')
+    return getFromEnv('bAssetReward')
   }
   bAssetGov(denom: string): string {
-    return "terra1"
-    throw new Error('Method not implemented.')
+    return getFromEnv('bAssetGov')
   }
   bAssetToken(denom: string): string {
-    return "terra1"
-    throw new Error('Method not implemented.')
+    return getFromEnv('anchorToken')
   }
   bAsset(denom: string): string {
-    return "terra1"
-    throw new Error('Method not implemented.')
+    return getFromEnv('bAsset')
   }
   market(denom: string): string {
-    return "terra1"
-    throw new Error('Method not implemented.')
+    return getFromEnv('mmMarket')
   }
   custody(): string {
-    return "terra1"
-    throw new Error('Method not implemented.')
+    return getFromEnv('mmCustody')
   }
   overseer(): string {
-    return "terra1"
-    throw new Error('Method not implemented.')
+    return getFromEnv('mmOverseer')
   }
   aToken(): string {
-    return "terra1"
-    throw new Error('Method not implemented.')
+    return getFromEnv('aUST')
   }
   oracle(): string {
-    return "terra1"
-    throw new Error('Method not implemented.')
+    return getFromEnv('mmOracle')
   }
+}
 
+function getFromEnv(key: string): string {
+  const val = process.env[reactifyEnv(key)]
+  if(typeof val === 'undefined') {
+    throw new Error(`address provider could not resolve key ${key}`)
+  }
+  return val
 }
