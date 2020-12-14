b6b4b5fb304deba52b5b2f93b846b91af225fde8
SSen <i@ssen.name>
2020-12-15 00:25:32 +0900

    feat: add app for design preview (workspace that implement the zeplin design files)

---
+++ design/src/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return <div>This app is a design preview</div>;
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
