a2f6e4fb45c01be2b3f279f978685d9c1c5e3617
SSen <i@ssen.name>
2020-12-14 23:52:04 +0900

    chore: add storybook settings

---
+++ app/.storybook/main.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "stories": [
+    '../src/**/*.stories.@(js|jsx|ts|tsx|mdx)'
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app"
+  ]
+}
