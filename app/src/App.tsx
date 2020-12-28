aea503a84d7847b0338b5a131091258379bb4dee
SSen <i@ssen.name>
2020-12-28 22:25:44 +0900

    chore: start refactoring data connection

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,8 +2,10 @@ import { AddressProviderFromEnvVar } from '@anchor-protocol/anchor-js/address-pr
 import { darkTheme } from '@anchor-protocol/neumorphism-ui/themes/darkTheme';
 import { GlobalStyle } from '@anchor-protocol/neumorphism-ui/themes/GlobalStyle';
 import { ThemeProvider } from '@anchor-protocol/neumorphism-ui/themes/ThemeProvider';
+import { ApolloProvider } from '@apollo/client';
 import { Footer } from 'components/Footer';
 import { Header } from 'components/Header';
+import { mantleClient } from 'env';
 import BassetBurn from 'pages/basset/burn';
 import BassetClaim from 'pages/basset/claim';
 import BassetMint from 'pages/basset/mint';
@@ -17,7 +19,6 @@ import {
   Switch,
 } from 'react-router-dom';
 import styled from 'styled-components';
-import { mantleConfig, MantleProvider } from './hooks/use-mantle';
 import { useWalletState, WalletProvider } from './hooks/use-wallet';
 import { AddressProviderProvider as ContractAddressProvider } from './providers/address-provider';
 
@@ -29,10 +30,14 @@ function AppBase({ className }: AppProps) {
   const wallet = useWalletState();
 
   return (
+    // React App routing
     <Router>
+      {/* Smart Contract Address */}
       <ContractAddressProvider value={new AddressProviderFromEnvVar()}>
+        {/* Terra Station Wallet Address */}
         <WalletProvider value={wallet} key={wallet.address}>
-          <MantleProvider client={mantleConfig}>
+          {/* Set GraphQL environenments */}
+          <ApolloProvider client={mantleClient}>
             <ThemeProvider theme={darkTheme}>
               <GlobalStyle />
               {/* Start Layout */}
@@ -51,7 +56,7 @@ function AppBase({ className }: AppProps) {
               </div>
               {/* End Layout */}
             </ThemeProvider>
-          </MantleProvider>
+          </ApolloProvider>
         </WalletProvider>
       </ContractAddressProvider>
     </Router>
