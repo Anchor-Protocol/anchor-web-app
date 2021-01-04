a1bc724b9e582be5d5709746462682a13d7abbc7
MSNTCS <najafi@codechain.io>
2021-01-04 18:58:05 +0900

    Add withdrawunbonded message

---
+++ packages/src/@anchor-protocol/anchor-js/fabricators/basset/basset-withdraw-unbonded.ts
@@ -0,0 +1,25 @@
+import { MsgExecuteContract } from '@terra-money/terra.js';
+import { validateInput } from '../../utils/validate-input';
+import { validateAddress } from '../../utils/validation/address';
+
+interface Option {
+  address: string;
+  bAsset: string;
+}
+
+export const fabricatebAssetWithdrawUnbonded = ({
+  address,
+  bAsset,
+}: Option) => (
+  addressProvider: AddressProvider.Provider,
+): MsgExecuteContract[] => {
+  validateInput([validateAddress(address)]);
+
+  const bAssetHubAddress = addressProvider.bAssetHub(bAsset);
+
+  return [
+    new MsgExecuteContract(address, bAssetHubAddress, {
+      withdraw_unbonded: {},
+    }),
+  ];
+};
