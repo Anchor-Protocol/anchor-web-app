00a7b23d7a837fb48823a23aebe5ee57700aeb5a
jess <jesse@terra.money>
2021-01-05 19:57:41 +0900

    fix(anchor-js/fabricators): remove redeem_all flag

--- packages/src/@anchor-protocol/anchor-js/fabricators/money-market/redeem-collateral.ts
+++ packages/src/@anchor-protocol/anchor-js/fabricators/money-market/redeem-collateral.ts
@@ -10,9 +10,7 @@ interface Option {
   address: string;
   market: string;
   borrower?: string;
-  // symbol: string,
-  redeem_all: boolean;
-  amount: number | null;
+  amount?: string;
 }
 
 /**
@@ -20,15 +18,13 @@ interface Option {
  * @param address Client’s Terra address.
  * @param market Type of stablecoin money market to redeem collateral.
  * @param symbol Symbol of collateral to redeem.
- * @param redeem_all Set this to true to redeem all symbol collateral deposited to loan_id.
  * @param amount (optional) Amount of collateral to redeem. Set this to null if redeem_all is true.
  * @param withdraw_to (optional) Terra address to withdraw redeemed collateral. If null, withdraws to address.
  */
 export const fabricateRedeemCollateral = ({
   address,
   market,
-  redeem_all = true,
-  amount = null,
+  amount,
 }: Option) => (
   addressProvider: AddressProvider.Provider,
 ): MsgExecuteContract[] => {
@@ -36,8 +32,6 @@ export const fabricateRedeemCollateral = ({
     validateAddress(address),
     validateWhitelistedMarket(market),
     amount ? validateIsGreaterThanZero(amount) : validateTrue,
-    // borrower ? validateAddress(borrower) : validateTrue,
-    // validateWhitelistedBAsset(symbol),
   ]);
 
   const mmOverseerContract = addressProvider.overseer(market.toLowerCase());
@@ -50,9 +44,9 @@ export const fabricateRedeemCollateral = ({
       unlock_collateral: [
         [
           address,
-          redeem_all
-            ? undefined
-            : new Int(new Dec(amount as number).mul(1000000)).toString(),
+          isAmountSet(amount)
+            ? new Int(new Dec(amount).mul(1000000)).toString()
+            : undefined,
         ],
       ],
     }),
@@ -61,10 +55,14 @@ export const fabricateRedeemCollateral = ({
     new MsgExecuteContract(address, custodyContract, {
       // @see https://github.com/Anchor-Protocol/money-market-contracts/blob/master/contracts/custody/src/msg.rs#L69
       withdraw_collateral: {
-        amount: redeem_all
-          ? undefined
-          : new Int(new Dec(amount as number).mul(1000000)).toString(),
+        amount: isAmountSet(amount)
+          ? new Int(new Dec(amount).mul(1000000)).toString()
+          : undefined,
       },
     }),
   ];
 };
+
+function isAmountSet(amount: string | undefined): amount is string {
+  return !!amount && !new Int(amount).isNaN();
+}
