081f6cd95b4c58fbd780d13e85541cb9ea8593c0
jess <jesse@terra.money>
2021-01-05 19:31:26 +0900

    fix(anchor-js/utils): fix number validation to use Int/Dec

--- packages/src/@anchor-protocol/anchor-js/utils/validation/number.ts
+++ packages/src/@anchor-protocol/anchor-js/utils/validation/number.ts
@@ -1,16 +1,17 @@
+import { Int } from '@terra-money/terra.js';
 import { InputEntry } from '../validate-input';
 
 export const validateIsNumber = (n: number | string): InputEntry => [
-  () => !Number.isNaN(n),
+  () => !new Int(n).isNaN(),
   `invalid number ${n}`,
 ];
 
 export const validateIsGreaterThanZero = (n: number | string): InputEntry => [
-  () => n > 0,
+  () => new Int(n).greaterThan(0),
   `number should be > 0.`,
 ];
 
 export const validateIsStringPrecision = (n: string): InputEntry => [
-  () => !Number.isNaN(n) && n.toString().split('.').length === 2,
+  () => !new Int(n).isNaN() && n.toString().split('.').length === 2,
   `number should be in precision format: ${n}.`,
 ];
