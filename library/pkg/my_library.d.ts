39ff43f8f3e596a59d9b8f920241a791f1cfd44e
SSen <i@ssen.name>
2021-01-16 17:08:18 +0900

    fix(wasm): include wasm out directory

---
+++ library/pkg/my_library.d.ts
@@ -0,0 +1,18 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {string} name
+*/
+export function greet(name: string): void;
+/**
+* @param {number} a
+* @param {number} b
+* @returns {number}
+*/
+export function plus(a: number, b: number): number;
+/**
+* @param {number} a
+* @param {number} b
+* @returns {number}
+*/
+export function minus(a: number, b: number): number;
---
+++ library/pkg/my_library.js
@@ -0,0 +1,2 @@
+import * as wasm from "./my_library_bg.wasm";
+export * from "./my_library_bg.js";
