c72ebc5283141a0c44eb4760874d3e93a5002e14
SSen <i@ssen.name>
2021-01-19 22:47:30 +0900

    fix(app, packages): build errors

--- app/src/components/Banner/__tests__/browserslist.test.ts
+++ app/src/components/Banner/__tests__/browserslist.test.ts
@@ -1,14 +1,14 @@
-import { matchesUA } from 'browserslist-useragent';
+import { getParser } from 'bowser';
 
 describe('browserslist', () => {
   test.each([
     'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_1_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36',
     'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_0_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4298.0 Safari/537.36',
   ])('should be user agent is chrome (%s)', (userAgent: string) => {
+    const browser = getParser(userAgent);
     expect(
-      matchesUA(userAgent, {
-        browsers: ['Chrome > 60'],
-        allowHigherVersions: true,
+      browser.satisfies({
+        chrome: '>60',
       }),
     ).toBeTruthy();
   });
