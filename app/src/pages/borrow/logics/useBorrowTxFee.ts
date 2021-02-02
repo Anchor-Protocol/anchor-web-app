4ed912b7cc9a96b5e8c9c585ef16f02022f475e8
SSen <i@ssen.name>
2021-02-02 18:03:55 +0900

    fix(app): borrow tx fee

--- app/src/pages/borrow/logics/useBorrowTxFee.ts
+++ app/src/pages/borrow/logics/useBorrowTxFee.ts
@@ -1,8 +1,11 @@
+import { min } from '@anchor-protocol/big-math';
 import { microfy, UST, uUST } from '@anchor-protocol/notation';
 import big, { Big } from 'big.js';
 import { Bank } from 'contexts/bank';
 import { FIXED_GAS } from 'env';
 import { useMemo } from 'react';
+
+// Tx_fee = MIN(User_Input/(1+tax_rate) * tax_rate , Max_tax) + Fixed_Gas
 
 export function useBorrowTxFee(borrowAmount: UST, bank: Bank) {
   return useMemo<uUST<Big> | undefined>(() => {
@@ -12,14 +15,12 @@ export function useBorrowTxFee(borrowAmount: UST, bank: Bank) {
 
     const amount = microfy(borrowAmount);
 
-    const userAmountTxFee = big(
-      amount.minus(amount).div(big(1).plus(bank.tax.taxRate)),
-    ).mul(bank.tax.taxRate);
+    const userAmountTxFee = big(amount.div(big(1).plus(bank.tax.taxRate))).mul(
+      bank.tax.taxRate,
+    );
 
-    if (userAmountTxFee.gt(bank.tax.maxTaxUUSD)) {
-      return big(bank.tax.maxTaxUUSD).plus(FIXED_GAS) as uUST<Big>;
-    } else {
-      return userAmountTxFee.plus(FIXED_GAS) as uUST<Big>;
-    }
+    return min(userAmountTxFee, bank.tax.maxTaxUUSD).plus(
+      FIXED_GAS,
+    ) as uUST<Big>;
   }, [borrowAmount, bank.tax.maxTaxUUSD, bank.tax.taxRate]);
 }
