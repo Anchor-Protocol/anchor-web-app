a623106914df7eb2d42f3b3a759891dff9875585
SSen <i@ssen.name>
2021-02-04 14:36:44 +0900

    feat(landing): webapp button icon

--- landing/src/components/Header/WebAppButton.tsx
+++ landing/src/components/Header/WebAppButton.tsx
@@ -1,11 +1,16 @@
 import styled from 'styled-components';
+import { Labtop } from '@anchor-protocol/icons';
 
 export interface WebAppButtonProps {
   className?: string;
 }
 
 function WebAppButtonBase({ className }: WebAppButtonProps) {
-  return <button className={className}>WebApp</button>;
+  return (
+    <button className={className}>
+      <Labtop /> WebApp
+    </button>
+  );
 }
 
 export const WebAppButton = styled(WebAppButtonBase)`
@@ -15,7 +20,14 @@ export const WebAppButton = styled(WebAppButtonBase)`
   border: 0;
   outline: none;
   cursor: pointer;
-  
+
   font-size: 11px;
   font-weight: 700;
+
+  svg {
+    margin-left: 4px;
+    margin-right: 9px;
+    font-size: 1em;
+    transform: scale(1.8) translateY(1px);
+  }
 `;
--- packages/src/@anchor-protocol/icons/index.ts
+++ packages/src/@anchor-protocol/icons/index.ts
@@ -3,6 +3,7 @@ import { ReactComponent as ArrowDownIcon } from './assets/ArrowDown.svg';
 import { ReactComponent as CircleArrowRightIcon } from './assets/CircleArrowRight.svg';
 import { ReactComponent as DiscordIcon } from './assets/Discord.svg';
 import { ReactComponent as WalletIcon } from './assets/Wallet.svg';
+import { ReactComponent as LabtopIcon } from './assets/Labtop.svg';
 import { createMaterialIcon } from './createMaterialIcon';
 
 export const Discord = createMaterialIcon(DiscordIcon);
@@ -10,3 +11,4 @@ export const Wallet = createMaterialIcon(WalletIcon);
 export const ArrowDown = createMaterialIcon(ArrowDownIcon);
 export const Anchor = createMaterialIcon(AnchorIcon);
 export const CircleArrowRight = createMaterialIcon(CircleArrowRightIcon);
+export const Labtop = createMaterialIcon(LabtopIcon);
