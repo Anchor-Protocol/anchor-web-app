a90f9193aadbd74e4a1e947a54e91c705e51cc14
SSen <i@ssen.name>
2021-02-15 11:59:48 +0900

    feat(app): progressive web app

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -2,10 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
 import reportWebVitals from './reportWebVitals';
+import { register } from './serviceWorkderRegistration';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+register();
 reportWebVitals();
