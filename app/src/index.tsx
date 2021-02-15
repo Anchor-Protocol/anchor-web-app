3cae3c5d2cee75817c525ffa625a62e7cee368c6
SSen <i@ssen.name>
2021-02-15 12:26:25 +0900

    feat(app): sentry

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,8 +1,18 @@
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
 import reportWebVitals from './reportWebVitals';
 import { register } from './serviceWorkderRegistration';
+
+Sentry.init({
+  dsn:
+    'https://f33dd06d6f5948bfb06d809d0d0a274c@o247107.ingest.sentry.io/5636828',
+  integrations: [new Integrations.BrowserTracing()],
+
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
