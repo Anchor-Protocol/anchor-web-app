8d3d28b6b3e3cec7db5efe45cbf8509d2df518f8
SSen <i@ssen.name>
2021-03-09 16:21:31 +0900

    fix(app): change minimum claimable rewards from 1ANC to 1uANC

--- app/src/pages/gov/env.ts
+++ app/src/pages/gov/env.ts
@@ -1,9 +1,7 @@
-import { MICRO } from '@anchor-protocol/notation';
-
 export const govPathname = 'gov';
 export const ancUstLpPathname = 'anc-ust-lp';
 export const ancGovernancePathname = 'anc-governance';
 export const ustBorrowPathname = 'ust-borrow';
 
-export const MINIMUM_CLAIM_BALANCE = MICRO;
+export const MINIMUM_CLAIM_BALANCE = 1;
 export const MAX_SPREAD = 0.1;
