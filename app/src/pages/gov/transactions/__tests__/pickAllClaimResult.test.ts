caf49a505c1b04fa8ddc72285f4b43bc8af37a0a
SSen <i@ssen.name>
2021-03-11 14:26:35 +0900

    test(app): expect result of gov all claim

---
+++ app/src/pages/gov/transactions/__tests__/pickAllClaimResult.test.ts
@@ -0,0 +1,20 @@
+import { uUST } from '@anchor-protocol/types';
+import { TxResult } from '@anchor-protocol/web-contexts/transactions/tx';
+import { pickAllClaimResult } from 'pages/gov/transactions/pickAllClaimResult';
+import json from './fixtures/claim-all.json';
+
+describe('pickAllClaimResult', () => {
+  test('should get result', () => {
+    const result = pickAllClaimResult({
+      txResult: json.txResult as TxResult,
+      txInfo: json.txInfo,
+      fixedGas: 350000 as uUST<number>,
+    });
+
+    expect(result.details[0]).toEqual({
+      name: 'Claimed',
+      value: '758.156075 ANC',
+    });
+    expect(result.details[2]).toEqual({ name: 'Tx Fee', value: '0.35 UST' });
+  });
+});
