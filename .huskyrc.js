15145ca1a0eadfe30c02b3f929959677082e8aea
SSen <i@ssen.name>
2021-03-12 23:27:44 +0900

    chore: downgrade husky for avoid license problem

---
+++ .huskyrc.js
@@ -0,0 +1,17 @@
+function precommit(...workspaces) {
+  return workspaces.map((workspace) => `yarn workspace ${workspace} run precommit`);
+}
+
+module.exports = {
+  hooks: {
+    'pre-commit': [
+      // auto source import every markdown documents
+      // @see https://www.npmjs.com/package/@handbook/markdown-source-import
+      //`markdown-source-import README.md --git-add`,
+      // see "lint-staged" on "package.json"
+      `lint-staged`,
+      // run workspaces precommit hook
+      //...precommit(`source`),
+    ].join(' && '),
+  },
+};
