943a3530f0dbd933ed076c85115fcdd89af95484
SSen <i@ssen.name>
2021-03-17 22:32:58 +0900

    fix(app): invalid number big.js

--- app/src/pages/earn/logics/validateWithdrawAmount.ts
+++ app/src/pages/earn/logics/validateWithdrawAmount.ts
@@ -1,7 +1,7 @@
 import { microfy } from '@anchor-protocol/notation';
 import type { UST, uUST } from '@anchor-protocol/types';
-import big, { Big, BigSource } from 'big.js';
 import { Bank } from 'base/contexts/bank';
+import big, { Big, BigSource } from 'big.js';
 import { ReactNode } from 'react';
 
 export function validateWithdrawAmount(
@@ -10,11 +10,9 @@ export function validateWithdrawAmount(
   totalDeposit: uUST<BigSource>,
   txFee: uUST<Big> | undefined,
 ): ReactNode {
-  const amount = microfy(withdrawAmount);
-
   if (withdrawAmount.length === 0) {
     return undefined;
-  } else if (amount.gt(totalDeposit)) {
+  } else if (microfy(withdrawAmount).gt(totalDeposit)) {
     return `Not enough aUST`;
   } else if (txFee && big(bank.userBalances.uUSD).lt(txFee)) {
     return `Not enough UST`;
