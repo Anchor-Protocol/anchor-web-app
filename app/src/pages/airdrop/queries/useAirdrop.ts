e72507b549116e2caa18d72539f9c5ac70abc4d6
jess <jesse@terra.money>
2021-03-23 10:47:58 +0900

    feat: airdrop eligibility check starting stage-2

--- app/src/pages/airdrop/queries/useAirdrop.ts
+++ app/src/pages/airdrop/queries/useAirdrop.ts
@@ -102,25 +102,47 @@ export function useAirdrop(): [Airdrop | 'in-progress' | null, () => void] {
 
   const refetch = useCallback(() => {
     if (walletReady && walletReady.network.chainID.startsWith('columbus')) {
-      queryIsClaimed(client, address, walletReady.walletAddress)
-        .then(({ data: { isClaimed } }) => {
-          if (isClaimed && !isClaimed.is_claimed) {
-            fetch(
-              `https://airdrop.anchorprotocol.com/api/get?address=${walletReady.walletAddress}&chainId=${walletReady.network.chainID}`,
-            )
-              .then((res) => res.json())
-              .then((airdrops: Airdrop[]) => {
-                console.log(airdrops);
-                const claimableAirdrops = airdrops.filter(
-                  ({ claimable }) => claimable,
+      // NOTE: temporary
+      // get airdrop list from db first,
+      // then do the contract check
+      fetch(
+        `https://airdrop.anchorprotocol.com/api/get?address=${walletReady.walletAddress}&chainId=${walletReady.network.chainID}`,
+      )
+        .then((res) => res.json())
+        .then(async (airdrops: Airdrop[]) => {
+          // parallel queries may result in mantle failure for now,
+          // do a series query
+          // TODO: make this check off-chain
+          return airdrops.reduce(
+            (t, airdrop) =>
+              t.then(async (filtered) => {
+                // cap filtered so that if any airdrop-eligible entry is found,
+                // do not care about the rest (saving network cost)
+                if (filtered.length > 0) return filtered;
+
+                const { stage } = airdrop;
+                const isClaimed = await queryIsClaimed(
+                  client,
+                  address,
+                  walletReady.walletAddress,
+                  stage,
                 );
 
-                if (claimableAirdrops.length > 0) {
-                  setAirdrop(claimableAirdrops[0]);
-                } else {
-                  setAirdrop(null);
-                }
-              });
+                return isClaimed.data.isClaimed &&
+                  isClaimed.data.isClaimed.is_claimed
+                  ? filtered // skip if already claimed
+                  : [...filtered, airdrop]; // eligible if not yet claimed
+              }),
+            Promise.resolve([] as Airdrop[]),
+          );
+        })
+        .then((airdrops: Airdrop[]) => {
+          // const claimableAirdrops = airdrops.filter(
+          //   ({ claimable }) => claimable,
+          // );
+
+          if (airdrops.length > 0) {
+            setAirdrop(airdrops[0]);
           } else {
             setAirdrop(null);
           }
@@ -151,6 +173,7 @@ export function queryIsClaimed(
   client: ApolloClient<any>,
   address: ContractAddress,
   walletAddress: HumanAddr,
+  stage: number,
 ): Promise<MappedApolloQueryResult<RawData, Data>> {
   return client
     .query<RawData, RawVariables>({
@@ -160,7 +183,7 @@ export function queryIsClaimed(
         airdropContract: address.bluna.airdropRegistry,
         isClaimedQuery: {
           is_claimed: {
-            stage: 1,
+            stage: stage,
             address: walletAddress,
           },
         },
