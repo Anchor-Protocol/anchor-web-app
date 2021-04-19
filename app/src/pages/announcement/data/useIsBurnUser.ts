61b263086a474cd188c7bdc6cff03e68cf4f25e5
SSen <i@ssen.name>
2021-04-19 15:48:03 +0900

    fix: update announcement address structure

--- app/src/pages/announcement/data/useIsBurnUser.ts
+++ app/src/pages/announcement/data/useIsBurnUser.ts
@@ -1,6 +1,12 @@
 import { HumanAddr, uToken } from '@anchor-protocol/types';
 import { useUserWallet } from '@anchor-protocol/wallet-provider';
-import burnData from './transformed/burn.json';
+import _burnData from './transformed/burn.json';
+
+const burnData: Record<HumanAddr, uToken> = {
+  ..._burnData,
+  // append test data
+  terra12hnhh5vtyg5juqnzm43970nh4fw42pt27nw9g9: '1000000',
+};
 
 export function useIsBurnUser(): { address: HumanAddr; amount: uToken } | null {
   const userWallet = useUserWallet();
@@ -11,15 +17,6 @@ export function useIsBurnUser(): { address: HumanAddr; amount: uToken } | null {
       address: userWallet.walletAddress,
       //@ts-ignore
       amount: burnData[userWallet.walletAddress],
-    };
-  }
-
-  if (
-    userWallet?.walletAddress === 'terra12hnhh5vtyg5juqnzm43970nh4fw42pt27nw9g9'
-  ) {
-    return {
-      address: userWallet.walletAddress,
-      amount: '100000' as uToken,
     };
   }
 
--- app/src/pages/announcement/data/useIsMintUser.ts
+++ app/src/pages/announcement/data/useIsMintUser.ts
@@ -1,6 +1,12 @@
 import { HumanAddr, uToken } from '@anchor-protocol/types';
 import { useUserWallet } from '@anchor-protocol/wallet-provider';
-import mintData from './transformed/mint.json';
+import _mintData from './transformed/mint.json';
+
+const mintData: Record<HumanAddr, uToken> = {
+  ..._mintData,
+  // append test data
+  terra12hnhh5vtyg5juqnzm43970nh4fw42pt27nw9g9: '1000000',
+};
 
 export function useIsMintUser(): { address: HumanAddr; amount: uToken } | null {
   const userWallet = useUserWallet();
@@ -11,15 +17,6 @@ export function useIsMintUser(): { address: HumanAddr; amount: uToken } | null {
       address: userWallet.walletAddress,
       //@ts-ignore
       amount: mintData[userWallet.walletAddress],
-    };
-  }
-
-  if (
-    userWallet?.walletAddress === 'terra12hnhh5vtyg5juqnzm43970nh4fw42pt27nw9g9'
-  ) {
-    return {
-      address: userWallet.walletAddress,
-      amount: '100000' as uToken,
     };
   }
 
