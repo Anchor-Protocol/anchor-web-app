433c9f6dd230e2dafc224e9e4588c256d043bffb
SSen <i@ssen.name>
2021-04-22 21:56:19 +0900

    fix: stringify sentry error

--- base/src/base/AppProviders.tsx
+++ base/src/base/AppProviders.tsx
@@ -39,7 +39,10 @@ import {
 } from './env';
 
 const operationBroadcasterErrorReporter =
-  process.env.NODE_ENV === 'production' ? captureException : undefined;
+  process.env.NODE_ENV === 'production'
+    ? (error: unknown) =>
+        captureException(error instanceof Error ? error.toString() : error)
+    : undefined;
 
 function Providers({ children }: { children: ReactNode }) {
   const {
