bed097ff69606fc4993235ea8537bd139ae62a5f
SSen <i@ssen.name>
2021-05-27 21:48:42 +0900

    feat: restore view an address on mobile device

--- app/src/components/Header/WalletSelector/ViewAddressButton.tsx
+++ app/src/components/Header/WalletSelector/ViewAddressButton.tsx
@@ -9,7 +9,7 @@ export interface ViewAddressButtonProps {
 function ViewAddressButtonBase({ className, onClick }: ViewAddressButtonProps) {
   return (
     <button className={className} onClick={onClick}>
-      <AccountBalanceWallet /> View an address
+      <AccountBalanceWallet /> <u>View an address</u>
     </button>
   );
 }
@@ -17,23 +17,27 @@ function ViewAddressButtonBase({ className, onClick }: ViewAddressButtonProps) {
 export const ViewAddressButton = styled(ViewAddressButtonBase)`
   display: inline-flex;
   text-decoration: none;
-  justify-content: center;
+  justify-content: flex-start;
   align-items: center;
 
-  width: 180px;
+  width: auto;
   height: 36px;
-  border-radius: 18px;
-  border: 0;
   outline: none;
   cursor: pointer;
+  text-align: left;
+  padding: 0;
 
-  font-size: 11px;
-  font-weight: 700;
+  background-color: transparent;
+  border: 0;
+  color: #666666;
+
+  font-size: 13px;
+  font-weight: 500;
 
   svg {
     margin-left: 4px;
     margin-right: 9px;
     font-size: 1em;
-    transform: scale(1.8);
+    transform: scale(1.4);
   }
 `;
