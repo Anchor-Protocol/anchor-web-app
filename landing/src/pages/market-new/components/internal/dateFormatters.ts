1e7194950fae67a01e8bf70ed985326186a4a8d9
SSen <i@ssen.name>
2021-06-01 11:19:45 +0900

    fix: chart x-axis label skip

--- landing/src/pages/market-new/components/internal/dateFormatters.ts
+++ landing/src/pages/market-new/components/internal/dateFormatters.ts
@@ -9,9 +9,18 @@ export function mediumDay(datetime: JSDateTime): string {
   return format(datetime, 'EEE, MMM d');
 }
 
+function isFirstItemAndNotNear1or15(i: number, timestamp: number): boolean {
+  if (i !== 0) {
+    return false;
+  }
+
+  const date = toDate(timestamp).getDate();
+  return date < 27 && date > 3;
+}
+
 export function xTimestampAixs(datetimes: JSDateTime[]): string[] {
   return datetimes.map((timestamp, i) => {
-    return i === 0 ||
+    return isFirstItemAndNotNear1or15(i, timestamp) ||
       isFirstDayOfMonth(timestamp) ||
       toDate(timestamp).getDate() === 15
       ? format(timestamp, 'MMM d')
