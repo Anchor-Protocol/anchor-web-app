f1a9eac886bd1b7a6fcf2a772489374ae69969dd
SSen <i@ssen.name>
2021-06-07 16:35:18 +0900

    feat: connect readonly function

--- packages/scripts/env.ts
+++ packages/scripts/env.ts
@@ -4,15 +4,15 @@ export const cwd = path.resolve(__dirname, '..')
 
 export const entry = {
   '@anchor-protocol/*': {
-    version: '0.15.0',
+    version: '0.15.1',
     tag: 'latest',
   },
   '@terra-money/*': {
-    version: '0.15.0',
-    tag: 'latest',
+    version: '0.15.1-alpha.2',
+    tag: 'canary',
   },
   '@terra-dev/*': {
-    version: '0.15.0',
-    tag: 'latest',
+    version: '0.15.1-alpha.2',
+    tag: 'canary',
   },
 };
