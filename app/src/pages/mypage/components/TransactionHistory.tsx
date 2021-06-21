047c566ec957711bcad98e8d4c3624efdc54c9bc
SSen <i@ssen.name>
2021-06-21 13:20:27 +0900

    feat: styling tx history component

--- app/src/pages/mypage/components/TransactionHistory.tsx
+++ app/src/pages/mypage/components/TransactionHistory.tsx
@@ -1,6 +1,12 @@
 import { Section } from '@terra-dev/neumorphism-ui/components/Section';
+import {
+  rulerLightColor,
+  rulerShadowColor,
+} from '@terra-dev/styled-neumorphism';
 import styled from 'styled-components';
 import React from 'react';
+import { screen } from 'env';
+import { CallMade } from '@material-ui/icons';
 
 export interface TransactionHistoryProps {
   className?: string;
@@ -10,17 +16,95 @@ function TransactionHistoryBase({ className }: TransactionHistoryProps) {
   return (
     <Section className={className}>
       <ul>
-        <li>...</li>
-        <li>...</li>
-        <li>...</li>
-        <li>...</li>
+        {Array.from({ length: 20 }, (_, i) => (
+          <li key={'txhistory' + i}>
+            <div>
+              <p>Bond</p>
+              <p>
+                Unbonded <strong>yyy bLUNA</strong> for{' '}
+                <strong>xxx LUNA</strong>{' '}
+                <sub>(completion time: 21.06.21 )</sub> <CallMade />
+              </p>
+            </div>
+            <time>Mon, Apr 19, 2021, 18:01 PM</time>
+          </li>
+        ))}
       </ul>
     </Section>
   );
 }
 
 export const StyledTransactionHistory = styled(TransactionHistoryBase)`
-  // TODO
+  ul {
+    padding: 0;
+    list-style: none;
+  }
+
+  li {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    padding: 12px 0;
+
+    cursor: pointer;
+
+    &:hover {
+      background-color: ${({ theme }) => theme.hoverBackgroundColor};
+    }
+
+    p:nth-child(1) {
+      font-size: 13px;
+      color: ${({ theme }) => theme.dimTextColor};
+    }
+
+    p:nth-child(2) {
+      font-size: 14px;
+
+      svg {
+        color: ${({ theme }) => theme.dimTextColor};
+        font-size: 1em;
+        vertical-align: bottom;
+      }
+    }
+
+    time {
+      font-size: 12px;
+      color: ${({ theme }) => theme.dimTextColor};
+    }
+
+    sub {
+      color: ${({ theme }) => theme.dimTextColor};
+      font-size: 0.8em;
+      vertical-align: unset;
+    }
+
+    &:not(:first-child) {
+      border-top: 1px solid
+        ${({ theme }) =>
+          rulerLightColor({
+            intensity: theme.intensity,
+            color: theme.backgroundColor,
+          })};
+    }
+
+    &:not(:last-child) {
+      border-bottom: 1px solid
+        ${({ theme }) =>
+          rulerShadowColor({
+            intensity: theme.intensity,
+            color: theme.backgroundColor,
+          })};
+    }
+  }
+
+  @media (max-width: ${screen.tablet.max}px) {
+    li {
+      flex-direction: column;
+      align-items: flex-start;
+      justify-content: flex-start;
+    }
+  }
 `;
 
 export const TransactionHistory =
--- app/src/pages/mypage/index.tsx
+++ app/src/pages/mypage/index.tsx
@@ -1,7 +1,8 @@
-import { PaddedLayout } from 'components/layouts/PaddedLayout';
+import { CenteredLayout } from 'components/layouts/CenteredLayout';
 import React from 'react';
 import styled from 'styled-components';
 import { TransactionHistory } from './components/TransactionHistory';
+import { screen } from 'env';
 
 export interface MypageProps {
   className?: string;
@@ -9,15 +10,32 @@ export interface MypageProps {
 
 function MypageBase({ className }: MypageProps) {
   return (
-    <PaddedLayout className={className}>
+    <CenteredLayout className={className} maxWidth={940}>
       <h1>Transaction History</h1>
       <TransactionHistory />
-    </PaddedLayout>
+    </CenteredLayout>
   );
 }
 
 export const StyledMypage = styled(MypageBase)`
-  // TODO
+  h1 {
+    font-size: 44px;
+    margin-bottom: 40px;
+  }
+
+  @media (max-width: ${screen.tablet.max}px) {
+    h1 {
+      font-size: 34px;
+      margin-bottom: 25px;
+    }
+  }
+
+  @media (max-width: ${screen.mobile.max}px) {
+    h1 {
+      font-size: 24px;
+      margin-bottom: 15px;
+    }
+  }
 `;
 
 export const Mypage =
