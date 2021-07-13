84a989224c81c3c62c5477fb4292f6ddd64f6401
SSen <i@ssen.name>
2021-07-13 13:16:00 +0900

    fix: borrowLimit formula

--- app/src/@anchor-protocol/webapp-fns/logics/borrow/computeBorrowLimit.ts
+++ app/src/@anchor-protocol/webapp-fns/logics/borrow/computeBorrowLimit.ts
@@ -1,23 +1,28 @@
 import { moneyMarket, uUST } from '@anchor-protocol/types';
-import big, { Big, BigSource } from 'big.js';
+import { sum, vectorMultiply } from '@terra-dev/big-math';
+import { Big, BigSource } from 'big.js';
 import { BAssetLtvs } from '../../queries/borrow/market';
+import { vectorizeBAssetMaxLtvs } from './vectorizeBAssetLtvs';
+import { vectorizeOraclePrices } from './vectorizeOraclePrices';
+import { vectorizeOverseerCollaterals } from './vectorizeOverseerCollaterals';
 
 export function computeBorrowLimit(
   overseerCollaterals: moneyMarket.overseer.CollateralsResponse,
   oraclePrices: moneyMarket.oracle.PricesResponse,
   bAssetLtvs: BAssetLtvs,
 ): uUST<BigSource> {
-  return overseerCollaterals.collaterals.reduce(
-    (total, [token, lockedAmount]) => {
-      const oracle = oraclePrices.prices.find(({ asset }) => asset === token);
-      const ltv = bAssetLtvs.get(token);
+  const vector = oraclePrices.prices.map(({ asset }) => asset);
+  const lockedAmounts = vectorizeOverseerCollaterals(
+    vector,
+    overseerCollaterals.collaterals,
+  );
+  const prices = vectorizeOraclePrices(vector, oraclePrices.prices);
+  const maxLtvs = vectorizeBAssetMaxLtvs(vector, Array.from(bAssetLtvs));
 
-      if (!oracle || !ltv) {
-        return total;
-      }
+  // sum([lockedAmounts] * [prices] * [maxLtvs])
 
-      return big(lockedAmount).mul(oracle.price).mul(ltv.max);
-    },
-    big(0),
-  ) as uUST<Big>;
+  const ustAmounts = vectorMultiply(lockedAmounts, prices);
+  const ustBorrowLimits = vectorMultiply(ustAmounts, maxLtvs);
+
+  return sum(...ustBorrowLimits) as uUST<Big>;
 }
