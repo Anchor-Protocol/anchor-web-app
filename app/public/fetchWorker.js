aa47bff88d20436d138f1ecbb9daa698581b115b
SSen <i@ssen.name>
2021-07-19 17:24:10 +0900

    fix: do not use spread operator and some esnext features for old browsers

--- app/public/fetchWorker.js
+++ app/public/fetchWorker.js
@@ -1,17 +1,22 @@
 onmessage = function (event) {
-  fetch(event.data.endpoint, {
-    ...event.data.requestInit,
-    method: 'POST',
-    headers: {
-      ...event.data.requestInit?.headers,
-      'Content-Type': 'application/json',
-      'Accept': 'application/json',
-    },
-    body: JSON.stringify({
-      query: event.data.query,
-      variables: event.data.variables,
+  fetch(
+    event.data.endpoint,
+    Object.assign({}, event.data.requestInit, {
+      method: 'POST',
+      headers: Object.assign(
+        {},
+        event.data.requestInit ? event.data.requestInit.headers : undefined,
+        {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+        },
+      ),
+      body: JSON.stringify({
+        query: event.data.query,
+        variables: event.data.variables,
+      }),
     }),
-  })
+  )
     .then((res) => res.json())
     .then(({ data, errors }) => {
       if (!!errors) {
