23fe5e62a5e7db7c9a359de8579b171f57be4c0f
SSen <i@ssen.name>
2021-08-03 21:01:38 +0900

    fix: wallet-provider update (related https://github.com/terra-money/wallet-provider/commit/e376caf57c41346925ad78970ed8d2026bb41987)

--- base/src/base/env.ts
+++ base/src/base/env.ts
@@ -136,4 +136,4 @@ export const screen = {
   // huge monitor : @media (min-width: ${screen.monitor.min}px)
 } as const;
 
-// build: trigger force build - 21.07.05
+// build: trigger force build - 21.08.03
