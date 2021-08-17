68a7551787e4b0b3c8098959253106d32d69b250
SSen <i@ssen.name>
2021-08-17 17:49:22 +0900

    fix: can't find variable: ResizeObserver from the chart.js

--- app/src/polyfills/polyfills.js
+++ app/src/polyfills/polyfills.js
@@ -1,5 +1,10 @@
 import * as buffer from 'buffer';
+import ResizeObserver from 'resize-observer-polyfill';
 import process from './process-es6';
 
 window.Buffer = buffer.Buffer;
 window.process = process;
+
+if (typeof window.ResizeObserver === 'undefined') {
+  window.ResizeObserver = ResizeObserver;
+}
