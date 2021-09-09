68eb47f88f748695348e4dd7ee0696ea3f2cf7c6
SSen <i@ssen.name>
2021-09-09 12:48:23 +0900

    fix: set window.ResizeObserver polyfill to solve chart.js errors

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,4 +1,10 @@
+import ResizeObserverPolyfill from 'resize-observer-polyfill';
+
 //@ts-ignore
 window.global = window;
+
+if (typeof window.ResizeObserver === 'undefined') {
+  window.ResizeObserver = ResizeObserverPolyfill;
+}
 
 import('@libs/vite-polyfills/polyfills').then(() => import('./main'));
