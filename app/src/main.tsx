d721940746de1de39c1d077e1324973b9783ce9a
SSen <i@ssen.name>
2021-09-12 11:28:44 +0900

    fix: do not send to Sentry few errors (UserDenied, ResizeObserver)

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -12,9 +12,14 @@ if (process.env.NODE_ENV === 'production' && typeof SENTRY_DSN === 'string') {
     //integrations: [new Integrations.BrowserTracing()],
     //tracesSampleRate: 1.0,
     maxValueLength: 800,
-    // may some lib (maybe chart.js) have problem this error in some browsers https://github.com/WICG/resize-observer/issues/38
-    // but, failed investigating. this error still investigation
-    ignoreErrors: ['ResizeObserver loop limit exceeded'],
+    ignoreErrors: [
+      // may some lib (maybe chart.js) have problem this error in some browsers https://github.com/WICG/resize-observer/issues/38
+      // but, failed investigating. this error still investigation
+      'ResizeObserver loop limit exceeded',
+      'ResizeObserver loop completed with undelivered notifications.',
+      // Ignore user actions
+      'User Denied',
+    ],
   });
 
   configErrorBoundary(Sentry.ErrorBoundary);
