17cc15fc68809513f09844bcb4c884330a69b144
SSen <i@ssen.name>
2021-11-17 13:10:49 +0900

    fix: remove duplicated call

--- app/src/@libs/app-provider/hooks/useRefetchQueries.ts
+++ app/src/@libs/app-provider/hooks/useRefetchQueries.ts
@@ -6,14 +6,13 @@ import { QueryRefetch } from '../types';
 function runRefetch(queryRefetch: string | QueryRefetch): Promise<string> {
   return new Promise<string>((resolve) => {
     if (typeof queryRefetch === 'string') {
-      // resolve(queryRefetch)
+      //resolve(queryRefetch)
       // FIXME <Ian Lee> Data that has not been updated
       //  at the time of Query call immediately after Tx completion is coming.
       //  In order to update the data normally, a delay of 200 ms is set.
       setTimeout(() => {
         resolve(queryRefetch);
       }, 200);
-      resolve(queryRefetch);
     } else if (typeof queryRefetch.wait === 'number') {
       setTimeout(() => {
         resolve(queryRefetch.queryKey);
