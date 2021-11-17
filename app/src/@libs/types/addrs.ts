8ffb4316b7a25814b5a572768be14f065f1e9a48
SSen <i@ssen.name>
2021-11-17 12:36:38 +0900

    fix: Data that has not been updated at the time of Query call immediately after Tx completion is coming. In order to update the data normally, a delay of 200 ms is set.

--- app/src/@libs/types/addrs.ts
+++ app/src/@libs/types/addrs.ts
@@ -41,6 +41,7 @@ export type WASMContractResult = {
 
 export namespace rs {
   export type u8 = number;
+  export type u16 = number;
   export type u32 = number;
   export type u64 = number;
   export type Uint128 = string;
