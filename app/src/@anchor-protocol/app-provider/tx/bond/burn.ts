fd63d254dd92d180d01559ffd2ccc044ef8125d5
SSen <i@ssen.name>
2021-12-09 17:08:42 +0900

    feat: change the gasWanted of bond / unbond

--- app/src/@anchor-protocol/app-provider/tx/bond/burn.ts
+++ app/src/@anchor-protocol/app-provider/tx/bond/burn.ts
@@ -36,7 +36,7 @@ export function useBondBurnTx() {
         network: connectedWallet.network,
         post: connectedWallet.post,
         fixedGas: fixedFee,
-        gasFee: constants.gasWanted,
+        gasFee: constants.bondGasWanted,
         gasAdjustment: constants.gasAdjustment,
         addressProvider,
         // query
@@ -53,7 +53,7 @@ export function useBondBurnTx() {
     [
       connectedWallet,
       fixedFee,
-      constants.gasWanted,
+      constants.bondGasWanted,
       constants.gasAdjustment,
       addressProvider,
       queryClient,
