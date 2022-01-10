03e51f772ebfe9bfd1767e5d0d2cf9ddee49cda3
SSen <i@ssen.name>
2022-01-10 17:06:25 +0900

    fix: decrease fixed fee

--- app/src/env.ts
+++ app/src/env.ts
@@ -71,7 +71,7 @@ export function ANCHOR_QUERY_CLIENT(network: NetworkInfo): 'lcd' | 'hive' {
 export function ANCHOR_CONSTANTS(network: NetworkInfo): AnchorConstants {
   return {
     gasWanted: 1_000_000 as Gas,
-    fixedGas: 1_971_053 as Gas,
+    fixedGas: 1_671_053 as Gas,
     blocksPerYear: 4_656_810,
     gasAdjustment: 1.6 as Rate<number>,
     airdropGasWanted: 300_000 as Gas,
