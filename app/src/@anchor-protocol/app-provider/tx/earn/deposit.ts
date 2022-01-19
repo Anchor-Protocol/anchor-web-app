f0a4f798e31ba2b128b490be717f2353c5f91e90
Cain O'Sullivan <cain.osullivan@gmail.com>
2022-01-19 11:23:03 +0800

    adding the AnchorAPI

--- app/src/@anchor-protocol/app-provider/tx/earn/deposit.ts
+++ app/src/@anchor-protocol/app-provider/tx/earn/deposit.ts
@@ -1,6 +1,7 @@
 import { useStream } from '@rx-stream/react';
 import { useAccount } from 'contexts/account';
-import { useAnchorApi } from 'contexts/api';
+import { AnchorDepositParams, useAnchorApi } from 'contexts/api';
+import { useCallback } from 'react';
 
 export function useEarnDepositTx() {
   const { connected } = useAccount();
@@ -8,6 +9,23 @@ export function useEarnDepositTx() {
   const { deposit } = useAnchorApi();
 
   const streamReturn = useStream(deposit);
+
+  return connected ? streamReturn : [null, null];
+}
+
+export function useEarnDepositTx2() {
+  const { connected } = useAccount();
+
+  const { deposit } = useAnchorApi();
+
+  const stream = useCallback(
+    (params: AnchorDepositParams) => {
+      return deposit(params);
+    },
+    [deposit],
+  );
+
+  const streamReturn = useStream(stream);
 
   return connected ? streamReturn : [null, null];
 }
