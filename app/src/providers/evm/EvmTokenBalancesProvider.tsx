dadce692d77fa012abb272ada911a221c66cce64
Stas Arsoba <stanislav.arsoba@gmail.com>
2022-01-20 15:50:03 +0100

    Fetch aUST balance in EvmTokenBalancesProvider

--- app/src/providers/evm/EvmTokenBalancesProvider.tsx
+++ app/src/providers/evm/EvmTokenBalancesProvider.tsx
@@ -5,24 +5,27 @@ import {
   AnchorTokenBalances,
   DefaultAnchorTokenBalances,
 } from '@anchor-protocol/app-fns';
-import { EVMAddr } from '@anchor-protocol/types';
 import { useEvmNativeBalance } from '../../@libs/app-provider/queries/evm/nativeBalances';
 import { useERC20Balance } from '../../@libs/app-provider/queries/erc20/balanceOf';
-import { u, UST, aUST } from '@anchor-protocol/types';
+import { UST, aUST, EVMAddr } from '@anchor-protocol/types';
 
 const EvmTokenBalancesProvider = ({ children }: UIElementProps) => {
   const ethBalance = useEvmNativeBalance();
   // TODO: Implement EVM address provider
-  // UST in ropsten
+  // UST (wh) in ropsten
   const ustBalance = useERC20Balance<UST>(
-    '0x6ca13a4ab78dd7d657226b155873a04db929a3a4' as EVMAddr,
+    '0x67d574b0218DcA2eB790b5922C5dA6A7b77E25a5' as EVMAddr,
+  );
+  // aUST in ropsten
+  const aUstBalance = useERC20Balance<aUST>(
+    '0x78D8036EB3Dcb4d8F099E9497d02CDaE202EA358' as EVMAddr,
   );
 
   const tokenBalances: AnchorTokenBalances = {
     ...DefaultAnchorTokenBalances,
     uUST: ustBalance,
     uEth: ethBalance,
-    uaUST: '876543' as u<aUST>,
+    uaUST: aUstBalance,
   };
   return (
     <TokenBalancesContext.Provider value={tokenBalances}>
