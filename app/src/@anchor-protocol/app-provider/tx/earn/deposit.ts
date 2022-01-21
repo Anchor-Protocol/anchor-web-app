59a44504838bbeabab70d618fc736fbeda504303
Cain O'Sullivan <cain.osullivan@gmail.com>
2022-01-21 14:40:54 +0800

    refactored the deposit API method for Terra

--- app/src/@anchor-protocol/app-provider/tx/earn/deposit.ts
+++ app/src/@anchor-protocol/app-provider/tx/earn/deposit.ts
@@ -1,7 +1,6 @@
 import { useStream } from '@rx-stream/react';
 import { useAccount } from 'contexts/account';
-import { AnchorDepositParams, useAnchorApi } from 'contexts/api';
-import { useCallback } from 'react';
+import { useAnchorApi } from 'contexts/api';
 
 export function useEarnDepositTx() {
   const { connected } = useAccount();
@@ -9,23 +8,6 @@ export function useEarnDepositTx() {
   const { deposit } = useAnchorApi();
 
   const streamReturn = useStream(deposit);
-
-  return connected ? streamReturn : [null, null];
-}
-
-export function useEarnDepositTx2() {
-  const { connected } = useAccount();
-
-  const { deposit } = useAnchorApi();
-
-  const stream = useCallback(
-    (params: AnchorDepositParams) => {
-      return deposit(params);
-    },
-    [deposit],
-  );
-
-  const streamReturn = useStream(stream);
 
   return connected ? streamReturn : [null, null];
 }
