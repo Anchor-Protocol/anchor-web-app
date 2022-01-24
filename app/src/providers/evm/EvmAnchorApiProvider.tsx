8f2604db8582e70a7b8afc71d7947860af42d553
Cain O'Sullivan <cain.osullivan@gmail.com>
2022-01-24 15:16:06 +0800

    WIP

--- app/src/providers/evm/EvmAnchorApiProvider.tsx
+++ app/src/providers/evm/EvmAnchorApiProvider.tsx
@@ -1,6 +1,10 @@
 import React, { useMemo } from 'react';
 import { UIElementProps } from '@libs/ui';
-import { AnchorApiContext, AnchorDepositParams } from 'contexts/api';
+import {
+  AnchorApiContext,
+  AnchorDepositParams,
+  AnchorWithdrawParams,
+} from 'contexts/api';
 import { Observable } from 'rxjs';
 import { pipe } from '@rx-stream/pipe';
 import { TxResultRendering, TxStreamPhase } from '@libs/app-fns';
@@ -26,9 +30,27 @@ const deposit = (
   return observable();
 };
 
+const withdraw = (
+  params: AnchorWithdrawParams,
+): Observable<TxResultRendering> => {
+  const observable = pipe<void, TxResultRendering>(() => {
+    return {
+      value: null,
+      phase: TxStreamPhase.SUCCEED,
+      receipts: [
+        {
+          name: 'Withdraw Amount',
+          value: '100.50 UST',
+        },
+      ],
+    };
+  });
+  return observable();
+};
+
 const EvmAnchorApiProvider = ({ children }: UIElementProps) => {
   const api = useMemo(() => {
-    return { deposit };
+    return { deposit, withdraw };
   }, []);
   return (
     <AnchorApiContext.Provider value={api}>
--- app/src/providers/terra/TerraAnchorApiProvider.tsx
+++ app/src/providers/terra/TerraAnchorApiProvider.tsx
@@ -1,67 +1,11 @@
 import React, { useMemo } from 'react';
 import { UIElementProps } from '@libs/ui';
-import { AnchorApiContext, AnchorDepositParams } from 'contexts/api';
-import { Observable } from 'rxjs';
-import { TxResultRendering } from '@libs/app-fns';
-import { earnDepositTx } from '@anchor-protocol/app-fns';
-import { ConnectedWallet, useConnectedWallet } from '@terra-money/use-wallet';
-import {
-  AnchorConstants,
-  ANCHOR_TX_KEY,
-  useAnchorWebapp,
-} from '@anchor-protocol/app-provider';
+import { AnchorApiContext } from 'contexts/api';
+import { useConnectedWallet } from '@terra-money/use-wallet';
+import { useAnchorWebapp } from '@anchor-protocol/app-provider';
 import { useRefetchQueries } from '@libs/app-provider';
-import { AddressProvider, MARKET_DENOMS } from '@anchor-protocol/anchor.js';
-import { QueryClient } from '@libs/query-client';
-
-type TxObservableFn<AnchorParams> = (
-  params: AnchorParams,
-) => Observable<TxResultRendering>;
-
-type TxObservableFnDependencies = {
-  constants: AnchorConstants;
-  connectedWallet: ConnectedWallet | undefined;
-  addressProvider: AddressProvider;
-  queryClient: QueryClient;
-  txErrorReporter?: (error: unknown) => string;
-  refetchQueries: (key: string) => void;
-};
-
-const createDepositApi = (
-  dependencies: TxObservableFnDependencies,
-): TxObservableFn<AnchorDepositParams> => {
-  const {
-    constants,
-    connectedWallet,
-    addressProvider,
-    queryClient,
-    txErrorReporter,
-    refetchQueries,
-  } = dependencies;
-  return (params: AnchorDepositParams) => {
-    if (!connectedWallet || connectedWallet.availablePost === false) {
-      throw new Error('Can not post!');
-    }
-    const { depositAmount, txFee, onTxSucceed } = params;
-    return earnDepositTx({
-      address: connectedWallet.walletAddress,
-      market: MARKET_DENOMS.UUSD,
-      amount: depositAmount,
-      network: connectedWallet.network,
-      post: connectedWallet.post,
-      txFee,
-      gasFee: constants.gasWanted,
-      gasAdjustment: constants.gasAdjustment,
-      addressProvider,
-      queryClient,
-      txErrorReporter,
-      onTxSucceed: () => {
-        onTxSucceed?.();
-        refetchQueries(ANCHOR_TX_KEY.EARN_DEPOSIT);
-      },
-    });
-  };
-};
+import { createDepositApi } from './api/createDepositApi';
+import { createWithdrawApi } from './api/createWithdrawApi';
 
 const TerraAnchorApiProvider = ({ children }: UIElementProps) => {
   const connectedWallet = useConnectedWallet();
@@ -82,6 +26,7 @@ const TerraAnchorApiProvider = ({ children }: UIElementProps) => {
     };
     return {
       deposit: createDepositApi(dependencies),
+      withdraw: createWithdrawApi(dependencies),
     };
   }, [
     connectedWallet,
