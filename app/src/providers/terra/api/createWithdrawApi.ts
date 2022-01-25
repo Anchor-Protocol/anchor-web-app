4f102cf576e86e7db4e10cd07078415de32105da
Cain O'Sullivan <cain.osullivan@gmail.com>
2022-01-25 10:58:27 +0800

    WIP

--- app/src/providers/terra/api/createWithdrawApi.ts
+++ app/src/providers/terra/api/createWithdrawApi.ts
@@ -1,74 +1,46 @@
-// return earnWithdrawTx({
-//   // fabricateMarketReedeemStableCoin
-//   address: connectedWallet.walletAddress,
-//   market: MARKET_DENOMS.UUSD,
-//   amount: withdrawAmount,
-//   // post
-//   network: connectedWallet.network,
-//   post: connectedWallet.post,
-//   txFee,
-//   gasFee: constants.gasWanted,
-//   gasAdjustment: constants.gasAdjustment,
-//   addressProvider,
-//   // query
-//   queryClient,
-//   // error
-//   txErrorReporter,
-//   // side effect
-//   onTxSucceed: () => {
-//     onTxSucceed?.();
-//     refetchQueries(ANCHOR_TX_KEY.EARN_WITHDRAW);
-//   },
-// });
-
-// export const withdraw = (
-//   params: AnchorWithdrawParams,
-// ): Observable<TxResultRendering> => {
-//   const observable = pipe<void, TxResultRendering>(() => {
-//     return {
-//       value: null,
-//       phase: TxStreamPhase.SUCCEED,
-//       receipts: [
-//         {
-//           name: 'Withdraw Amount',
-//           value: '100.50 UST',
-//         },
-//       ],
-//     };
-//   });
-//   return observable();
-// };
-
-//import { TxResultRendering, TxStreamPhase } from '@libs/app-fns';
+import { MARKET_DENOMS } from '@anchor-protocol/anchor.js';
+import { earnWithdrawTx } from '@anchor-protocol/app-fns';
 import { AnchorWithdrawParams } from 'contexts/api';
-//import { pipe } from 'rxjs';
+import { ANCHOR_TX_KEY } from '@anchor-protocol/app-provider';
 import { TxObservableFnDependencies, TxObservableFn } from './common';
 
 export const createWithdrawApi = (
   dependencies: TxObservableFnDependencies,
 ): TxObservableFn<AnchorWithdrawParams> => {
-  // const {
-  //   // constants,
-  //   // connectedWallet,
-  //   // addressProvider,
-  //   // queryClient,
-  //   // txErrorReporter,
-  //   // refetchQueries,
-  // } = dependencies;
+  const {
+    constants,
+    connectedWallet,
+    addressProvider,
+    queryClient,
+    txErrorReporter,
+    refetchQueries,
+  } = dependencies;
   return (params: AnchorWithdrawParams) => {
-    // const observable = pipe<void, TxResultRendering>(() => {
-    //   return {
-    //     value: null,
-    //     phase: TxStreamPhase.SUCCEED,
-    //     receipts: [
-    //       {
-    //         name: 'Withdraw Amount',
-    //         value: '100.50 UST',
-    //       },
-    //     ],
-    //   };
-    // });
-    // return observable();
-    throw new Error('');
+    if (!connectedWallet || !connectedWallet.availablePost) {
+      throw new Error('Can not post!');
+    }
+    const { withdrawAmount, txFee, onTxSucceed } = params;
+    return earnWithdrawTx({
+      // fabricateMarketReedeemStableCoin
+      address: connectedWallet.walletAddress,
+      market: MARKET_DENOMS.UUSD,
+      amount: withdrawAmount,
+      // post
+      network: connectedWallet.network,
+      post: connectedWallet.post,
+      txFee,
+      gasFee: constants.gasWanted,
+      gasAdjustment: constants.gasAdjustment,
+      addressProvider,
+      // query
+      queryClient,
+      // error
+      txErrorReporter,
+      // side effect
+      onTxSucceed: () => {
+        onTxSucceed?.();
+        refetchQueries(ANCHOR_TX_KEY.EARN_WITHDRAW);
+      },
+    });
   };
 };
