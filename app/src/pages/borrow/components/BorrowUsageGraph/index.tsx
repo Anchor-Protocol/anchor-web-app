03c2817663df6e4766241db22fb9bedff69e6a06
Cain O'Sullivan <cain.osullivan@gmail.com>
2022-02-01 20:09:13 +0800

    WIP

--- app/src/pages/borrow/components/BorrowUsageGraph/index.tsx
+++ app/src/pages/borrow/components/BorrowUsageGraph/index.tsx
@@ -1,78 +1,109 @@
-// import { formatUSTWithPostfixUnits } from '@anchor-protocol/notation';
-// import { Rate, u, UST } from '@anchor-protocol/types';
-// import { demicrofy, formatRate } from '@libs/formatter';
-// import { HorizontalGraphBar } from '@libs/neumorphism-ui/components/HorizontalGraphBar';
-// import { IconSpan } from '@libs/neumorphism-ui/components/IconSpan';
-// import { InfoTooltip } from '@libs/neumorphism-ui/components/InfoTooltip';
-// import big, {BigSource } from 'big.js';
+import { formatUSTWithPostfixUnits } from '@anchor-protocol/notation';
+import { u, UST } from '@anchor-protocol/types';
+import { demicrofy } from '@libs/formatter';
+import { HorizontalGraphBar } from '@libs/neumorphism-ui/components/HorizontalGraphBar';
+import { IconSpan } from '@libs/neumorphism-ui/components/IconSpan';
+import { InfoTooltip } from '@libs/neumorphism-ui/components/InfoTooltip';
+import big from 'big.js';
+import { UIElementProps } from 'components/layouts/UIElementProps';
+import { fixHMR } from 'fix-hmr';
 import React from 'react';
 // import { useMediaQuery } from 'react-responsive';
-// import { useTheme } from 'styled-components';
-// import { GraphLabel } from './GraphLabel';
-// import {
-//   colorFunction,
-//   labelRenderer,
-//   RenderData,
-//   valueFunction,
-// } from './render';
+import styled from 'styled-components';
+import { GraphLabel } from './GraphLabel';
+import {
+  colorFunction,
+  labelRenderer,
+  RenderData,
+  valueFunction,
+} from './render';
 
-const BorrowUsageGraph = () => {
-  // const borrowLimit = "123456789000" as u<UST>;
+function BorrowUsageGraphBase({ className }: UIElementProps) {
+  const borrowLimit = '1234567890' as u<UST>;
 
-  // const data = [
-  //   // {
-  //   //   position: 'top-marker',
-  //   //   label: `${formatRate(maxLtv)}% LTV${isSmallScreen ? '' : ' (MAX)'}`,
-  //   //   color: 'rgba(0, 0, 0, 0)',
-  //   //   textAlign: 'right',
-  //   //   value: maxLtv.toNumber(),
-  //   //   tooltip:
-  //   //     'Maximum allowed loan to value (LTV) ratio, collaterals will be liquidated when the LTV is bigger than this value.',
-  //   // },
-  //   // {
-  //   //   position: 'top-marker',
-  //   //   label: `${formatRate(safeLtv)}% LTV`,
-  //   //   color: 'rgba(0, 0, 0, 0)',
-  //   //   textAlign: 'right',
-  //   //   value: big(safeLtv).toNumber(),
-  //   //   tooltip: 'Recommended LTV',
-  //   // }
+  const data: RenderData[] = [
+    // {
+    //   position: 'top-marker',
+    //   label: `${formatRate(maxLtv)}% LTV${isSmallScreen ? '' : ' (MAX)'}`,
+    //   color: 'rgba(0, 0, 0, 0)',
+    //   textAlign: 'right',
+    //   value: maxLtv.toNumber(),
+    //   tooltip:
+    //     'Maximum allowed loan to value (LTV) ratio, collaterals will be liquidated when the LTV is bigger than this value.',
+    // },
+    // {
+    //   position: 'top-marker',
+    //   label: `${formatRate(safeLtv)}% LTV`,
+    //   color: 'rgba(0, 0, 0, 0)',
+    //   textAlign: 'right',
+    //   value: big(safeLtv).toNumber(),
+    //   tooltip: 'Recommended LTV',
+    // }
 
-  //   {
-  //     position: 'top-marker',
-  //     label: `${formatRate("1" as Rate<BigSource>)}% LTV`,
-  //     color: 'rgba(0, 0, 0, 0)',
-  //     textAlign: 'right',
-  //     value: big(1).toNumber(),
-  //     tooltip: 'Recommended LTV',
-  //   }
-  // ];
+    {
+      position: 'top-marker',
+      label: `75%`,
+      color: 'rgba(0, 0, 0, 0)',
+      textAlign: 'center',
+      value: big(75).toNumber(),
+      tooltip: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    },
+    {
+      position: 'top-marker',
+      label: `100%`,
+      color: 'rgba(0, 0, 0, 0)',
+      textAlign: 'right',
+      value: big(100).toNumber(),
+      tooltip: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    },
+    {
+      position: 'top',
+      label: '',
+      color: 'red',
+      value: 30,
+      tooltip: undefined,
+    },
+  ];
 
   return (
-    <div>asdklj</div>
-    // <HorizontalGraphBar<RenderData>
-    //   min={0}
-    //   max={100}
-    //   animate
-    //   data={data}
-    //   colorFunction={colorFunction}
-    //   valueFunction={valueFunction}
-    //   labelRenderer={labelRenderer}>
-    //   <GraphLabel style={{ left: 0 }}>
-    //     <IconSpan>
-    //       LTV <InfoTooltip>Loan-to-value ratio</InfoTooltip>
-    //     </IconSpan>
-    //   </GraphLabel>
-    //   <GraphLabel style={{ right: 0 }}>
-    //     <IconSpan>
-    //       Borrow Limit: ${formatUSTWithPostfixUnits(demicrofy(borrowLimit))}{' '}
-    //       <InfoTooltip>
-    //         The maximum amount of liability permitted from deposited collaterals
-    //       </InfoTooltip>
-    //     </IconSpan>
-    //   </GraphLabel>
-    // </HorizontalGraphBar>
+    <div className={className}>
+      <h3>
+        <IconSpan>
+          BORROW USAGE{' '}
+          <InfoTooltip>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </InfoTooltip>
+        </IconSpan>
+      </h3>
+      <HorizontalGraphBar<RenderData>
+        min={0}
+        max={100}
+        animate
+        data={data}
+        colorFunction={colorFunction}
+        valueFunction={valueFunction}
+        labelRenderer={labelRenderer}
+      >
+        <GraphLabel style={{ right: 0 }}>
+          <IconSpan>
+            Borrow Limit: ${formatUSTWithPostfixUnits(demicrofy(borrowLimit))}{' '}
+            <InfoTooltip>
+              The maximum amount of liability permitted from deposited
+              collaterals
+            </InfoTooltip>
+          </IconSpan>
+        </GraphLabel>
+      </HorizontalGraphBar>
+    </div>
   );
-};
+}
 
-export { BorrowUsageGraph };
+const StyledComponent = styled(BorrowUsageGraphBase)`
+  h3 {
+    font-size: 12px;
+    font-weight: 500;
+    margin-bottom: 12px;
+  }
+`;
+
+export const BorrowUsageGraph = fixHMR(StyledComponent);
