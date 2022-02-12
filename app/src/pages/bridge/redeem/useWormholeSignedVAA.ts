213787fcd71b82e8d2cee91796cd428cd816472e
Cain O'Sullivan <cain.osullivan@gmail.com>
2022-02-12 15:01:55 +0800

    working on wormhole redemption page

--- app/src/pages/bridge/redeem/useWormholeSignedVAA.ts
+++ app/src/pages/bridge/redeem/useWormholeSignedVAA.ts
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import {
   CHAIN_ID_ETHEREUM_ROPSTEN,
+  getEmitterAddressEth,
   getSignedVAAWithRetry,
 } from '@certusone/wormhole-sdk';
 
@@ -9,8 +10,9 @@ type WormholeSignedVAAReturn = {
   vaaBytes?: Uint8Array;
 };
 
+// need to make these work from the chain specifications, maybe we need
+// a chain configuration where all this information is stored
 const EMITTER_ADDRESS = '0xF174F9A837536C449321df1Ca093Bb96948D5386';
-
 const WORMHOLE_RPC_HOSTS = ['https://wormhole-v2-testnet-api.certus.one'];
 
 const useWormholeSignedVAA = (
@@ -23,11 +25,16 @@ const useWormholeSignedVAA = (
 
   useEffect(() => {
     (async () => {
+      //const emitterAddress = getEmitterAddressEth(getTokenBridgeAddressForChain(CHAIN_ID_ETHEREUM_ROPSTEN));
+      const emitterAddress = getEmitterAddressEth(EMITTER_ADDRESS);
       const vaa = await getSignedVAAWithRetry(
         WORMHOLE_RPC_HOSTS,
         CHAIN_ID_ETHEREUM_ROPSTEN,
-        EMITTER_ADDRESS,
+        emitterAddress,
         sequence,
+        undefined,
+        undefined,
+        100,
       );
       setResponse({
         loading: false,
