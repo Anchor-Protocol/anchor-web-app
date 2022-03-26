b4b76c4ff7bc904a6fcbc329f31e8056f8a8efaf
simke9445 <simke9445@gmail.com>
2022-03-26 03:30:43 +0100

    Make evm sdk only source of truth for contracts config.

--- app/src/providers/evm/EvmBalancesProvider.tsx
+++ app/src/providers/evm/EvmBalancesProvider.tsx
@@ -4,21 +4,18 @@ import { BalancesContext } from 'contexts/balances';
 import { AnchorBalances } from '@anchor-protocol/app-fns';
 import { useEvmNativeBalance } from '../../@libs/app-provider/queries/evm/nativeBalances';
 import { useERC20Balance } from '../../@libs/app-provider/queries/erc20/balanceOf';
-import { u, UST, aUST, Native, ANC, ERC20Addr } from '@anchor-protocol/types';
-import { useEvmWallet } from '@libs/evm-wallet';
-import { getAddress } from 'configurations/evm/addresses';
-import { EvmChainId } from '@anchor-protocol/crossanchor-sdk';
+import { u, UST, aUST, Native, ANC } from '@anchor-protocol/types';
+import { useEvmCrossAnchorSdk } from 'crossanchor';
 
 const EvmBalancesProvider = ({ children }: UIElementProps) => {
-  const { chainId = EvmChainId.ETHEREUM_ROPSTEN } = useEvmWallet();
-
   const native = useEvmNativeBalance();
+  const evmSdk = useEvmCrossAnchorSdk();
 
-  const ust = useERC20Balance<UST>(getAddress<ERC20Addr>(chainId, 'UST'));
+  const ust = useERC20Balance<UST>(evmSdk.config.token.UST);
 
-  const aUST = useERC20Balance<aUST>(getAddress<ERC20Addr>(chainId, 'aUST'));
+  const aUST = useERC20Balance<aUST>(evmSdk.config.token.aUST);
 
-  const ANC = useERC20Balance<ANC>(getAddress<ERC20Addr>(chainId, 'ANC'));
+  const ANC = useERC20Balance<ANC>(evmSdk.config.token.ANC);
 
   const balances = useMemo<AnchorBalances>(() => {
     return {
