6a49792be929f8b1043c38c477bb5bbfbfd55e89
Rodion Chachura <geekrodion@gmail.com>
2022-04-04 11:38:43 +0400

    use activate in switchNetwork

--- app/src/@libs/evm-wallet/hooks/useSwitchNetwork.ts
+++ app/src/@libs/evm-wallet/hooks/useSwitchNetwork.ts
@@ -4,9 +4,7 @@ import {
   useDeploymentTarget,
 } from '@anchor-protocol/app-provider';
 import { EvmChainId } from '@anchor-protocol/crossanchor-sdk';
-import { ethers } from 'ethers';
 import { useCallback } from 'react';
-import { chainConfigurations } from '../constants';
 import { useEvmWallet } from './useEvmWallet';
 
 export const evmChainId = (chain: Chain) => {
@@ -21,43 +19,21 @@ export const evmChainId = (chain: Chain) => {
 export const useSwitchNetwork = () => {
   const { updateTarget } = useDeploymentTarget();
 
-  const { provider } = useEvmWallet();
+  const {
+    actions: { activate },
+    connectType,
+  } = useEvmWallet();
 
   return useCallback(
-    (target: DeploymentTarget) => {
+    async (target: DeploymentTarget) => {
       const targetChainId = target.evmChainId ?? evmChainId(target.chain);
 
-      if (target.isEVM && provider?.provider?.isMetaMask) {
-        if (provider?.provider?.request) {
-          provider.provider
-            .request({
-              method: 'wallet_switchEthereumChain',
-              params: [{ chainId: ethers.utils.hexValue(targetChainId) }],
-            })
-            .then(() => {
-              updateTarget({ ...target, evmChainId: targetChainId });
-            })
-            .catch(async (error: Error & { code: number }) => {
-              if (error.code === 4902) {
-                // 4902 = user attempting to switch to an unrecognized chainId in his wallet
-                if (
-                  chainConfigurations[targetChainId] &&
-                  provider?.provider?.request
-                ) {
-                  provider.provider.request({
-                    method: 'wallet_addEthereumChain',
-                    params: [chainConfigurations[targetChainId]],
-                  });
-                }
-              }
-            });
-
-          return;
-        }
+      if (target.isEVM) {
+        await activate(connectType, targetChainId);
       }
 
       updateTarget({ ...target, evmChainId: targetChainId });
     },
-    [provider, updateTarget],
+    [activate, connectType, updateTarget],
   );
 };
