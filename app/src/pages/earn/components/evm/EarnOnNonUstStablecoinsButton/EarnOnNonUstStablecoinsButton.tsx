2eae118e9f2a633020c60c717560d4084d13cfbe
Cain O'Sullivan <cain.osullivan@gmail.com>
2022-04-14 10:14:57 +0800

    Feature/earn in non ust (#335)
    * Added links for Earning in Non-UST

---
+++ app/src/pages/earn/components/evm/EarnOnNonUstStablecoinsButton/EarnOnNonUstStablecoinsButton.tsx
@@ -0,0 +1,38 @@
+import { ButtonBaseProps } from '@material-ui/core';
+import { BorderButton } from '@libs/neumorphism-ui/components/BorderButton';
+import React from 'react';
+import styled from 'styled-components';
+import { DollarCoin } from '@anchor-protocol/icons';
+import { useEarnOnNonUstStablecoinsDialog } from './useEarnOnNonUstStablecoinsDialog';
+
+export function EarnOnNonUstStablecoinsButton(buttonProps: ButtonBaseProps) {
+  const [openDialog, dialogElement] = useEarnOnNonUstStablecoinsDialog();
+
+  return (
+    <>
+      <Button {...buttonProps} onClick={() => openDialog({})}>
+        <DollarCoin />
+        <span>Earn on Non-UST Stablecoins</span>
+      </Button>
+      {dialogElement}
+    </>
+  );
+}
+
+const Button = styled(BorderButton)`
+  height: initial;
+  padding: 7px 10px;
+  border: none !important;
+  font-size: 16px;
+  line-height: 20px;
+
+  svg {
+    font-size: 1em;
+    transform: scale(1.2);
+    margin-right: 8px;
+  }
+
+  @media (min-width: 701px) and (max-width: 1050px) {
+    max-width: 190px;
+  }
+`;
---
+++ app/src/pages/earn/components/evm/EarnOnNonUstStablecoinsButton/useEarnOnNonUstStablecoinsDialog.tsx
@@ -0,0 +1,148 @@
+import { Modal } from '@material-ui/core';
+import { Launch } from '@material-ui/icons';
+import { Dialog } from '@libs/neumorphism-ui/components/Dialog';
+import { EmbossButton } from '@libs/neumorphism-ui/components/EmbossButton';
+import { DialogProps, OpenDialog, useDialog } from '@libs/use-dialog';
+import React, { ReactNode } from 'react';
+import styled, { css } from 'styled-components';
+import orion from '../../assets/orion.svg';
+import curve from '../../assets/curve.svg';
+import { Chain, useDeploymentTarget } from '@anchor-protocol/app-provider';
+
+interface FormParams {
+  className?: string;
+}
+
+type FormReturn = void;
+
+export function useEarnOnNonUstStablecoinsDialog(): [
+  OpenDialog<FormParams, FormReturn>,
+  ReactNode,
+] {
+  return useDialog(Component);
+}
+
+function ComponentBase({
+  className,
+  closeDialog,
+}: DialogProps<FormParams, FormReturn>) {
+  const {
+    target: { chain },
+  } = useDeploymentTarget();
+
+  return (
+    <Modal open onClose={() => closeDialog()}>
+      <Dialog className={className} onClose={() => closeDialog()}>
+        <h1>Earn on Non-UST Stablecoins</h1>
+
+        {chain === Chain.Ethereum && (
+          <section>
+            <h2>Earn</h2>
+            <EmbossButton
+              component="a"
+              href="https://app.orion.money/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <span>
+                Orion Money&nbsp;
+                <sub>
+                  <Launch />
+                </sub>
+              </span>
+              <i>
+                <img src={orion} alt="Orion Money" />
+              </i>
+            </EmbossButton>
+          </section>
+        )}
+
+        <section>
+          <h2>Swaps</h2>
+          <EmbossButton
+            component="a"
+            href={
+              chain === Chain.Ethereum
+                ? 'https://curve.fi/'
+                : 'https://avax.curve.fi/'
+            }
+            target="_blank"
+            rel="noreferrer"
+          >
+            <span>
+              Curve&nbsp;
+              <sub>
+                <Launch />
+              </sub>
+            </span>
+            <i>
+              <img src={curve} alt="Curve" />
+            </i>
+          </EmbossButton>
+        </section>
+      </Dialog>
+    </Modal>
+  );
+}
+
+export const dialogStyle = css`
+  h1 {
+    font-size: 24px;
+    text-align: center;
+    font-weight: 300;
+    margin-bottom: 41px;
+    letter-spacing: -0.5px;
+  }
+
+  h2 {
+    font-size: 12px;
+    font-weight: 600;
+    color: ${({ theme }) => theme.dimTextColor};
+    margin-bottom: 8px;
+    line-height: 1;
+  }
+
+  section:first-of-type {
+    margin-bottom: 32px;
+  }
+
+  a {
+    width: 100%;
+    height: 60px;
+    font-size: 16px;
+    padding: 0 37px 0 27px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    span {
+      sub {
+        vertical-align: bottom;
+        font-size: 1ex;
+
+        svg {
+          font-size: 1em;
+          color: ${({ theme }) => theme.dimTextColor};
+          transform-origin: bottom;
+          transform: scale(1.5) translateX(0.1em);
+        }
+      }
+    }
+
+    i {
+      width: 40px;
+      text-align: center;
+      transform: translateY(1px);
+    }
+
+    &:not(:first-of-type) {
+      margin-top: 16px;
+    }
+  }
+`;
+
+const Component = styled(ComponentBase)`
+  width: 458px;
+
+  ${dialogStyle};
+`;
