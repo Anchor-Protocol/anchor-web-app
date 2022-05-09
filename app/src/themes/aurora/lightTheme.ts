44e1f6603d4fae6b5b76680771510e2b122279bd
Rodion Chachura <geekrodion@gmail.com>
2022-05-09 07:45:42 +0400

    update aurora buttons colors

--- app/src/themes/aurora/lightTheme.ts
+++ app/src/themes/aurora/lightTheme.ts
@@ -4,15 +4,16 @@ import {
   LIGHT_GRAY_300,
   BLACK,
   GRAY_400,
+  WHITE,
 } from '../lightThemeDefault';
 
 const PRIMARY_950 = '#120523';
 const PRIMARY_900 = '#3B0A4A';
 // const PRIMARY_800 = '#781276';
-const PRIMARY_700 = '#A51A89';
+// const PRIMARY_700 = '#A51A89';
 const PRIMARY_600 = '#DD25A4';
 const PRIMARY_500 = '#F82EAE';
-const PRIMARY_400 = '#FD49BE';
+// const PRIMARY_400 = '#FD49BE';
 // const PRIMARY_300 = '#FE71DD';
 const PRIMARY_200 = '#FF9CED';
 const PRIMARY_100 = '#FED2FF';
@@ -29,8 +30,6 @@ const SECONDARY_500 = '#8FD460';
 
 const POSITIVE = '#1969FF';
 const NEGATIVE = '#EC6597';
-
-/* eslint-enable */
 
 export const lightTheme: DefaultTheme = {
   ...defaultLightTheme,
@@ -56,7 +55,7 @@ export const lightTheme: DefaultTheme = {
   ],
   header: {
     ...defaultLightTheme.header,
-    backgroundColor: 'linear-gradient(109.79deg, #FF26A8 0%, #8698FF 99.99%)',
+    backgroundColor: PRIMARY_500,
     textColor: PRIMARY_950,
   },
   messageBox: {
@@ -68,14 +67,14 @@ export const lightTheme: DefaultTheme = {
   actionButton: {
     backgroundColor: PRIMARY_950,
     backgroundHoverColor: PRIMARY_900,
-    textColor: PRIMARY_500,
-    hoverTextColor: PRIMARY_400,
+    textColor: WHITE,
+    hoverTextColor: WHITE,
   },
   borderButton: {
-    borderColor: PRIMARY_600,
-    borderHoverColor: PRIMARY_700,
-    textColor: PRIMARY_600,
-    hoverTextColor: PRIMARY_700,
+    borderColor: PRIMARY_950,
+    borderHoverColor: PRIMARY_900,
+    textColor: PRIMARY_950,
+    hoverTextColor: PRIMARY_900,
     hoverBackgroundColor: 'transparent',
   },
   tooltip: {
